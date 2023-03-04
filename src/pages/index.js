00844c316db3e910196fd7d76116d9715500f603
Kumara002 <kumaramurugan002@gmail.com>
2023-03-04 05:57:55 +0530

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,6 @@ const Home = () => (
                    data-item-price={product.price}
                    data-item-image={product.image.url}
                    data-item-name={product.name}
-                   data-item-quantity={product.quantity}
                    data-item-url={`https://gatsbydatacmssnipcartmaind7f7.gatsbyjs.io/`}>Add to card</button>
                 </div>
               </div>
